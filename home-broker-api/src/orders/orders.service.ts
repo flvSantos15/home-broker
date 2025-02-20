cde68a55b491fda58473936520ce503bbf67b43b
flvsantos15 <flvsantos300@gmail.com>
2025-02-19 22:58:18 -0300

    chore: add orders page

--- home-broker-api/src/orders/orders.service.ts
+++ home-broker-api/src/orders/orders.service.ts
@@ -3,6 +3,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Order, OrderStatus } from './entities/order.entity';
 import { Model } from 'mongoose';
+import { Asset } from 'src/assets/entities/asset.entity';
 
 @Injectable()
 export class OrdersService {
@@ -21,7 +22,10 @@ export class OrdersService {
   }
 
   findAll(filter: { walletId: string }) {
-    return this.orderSchema.find({ wallet: filter.walletId });
+    return this.orderSchema
+      .find({ wallet: filter.walletId })
+      .populate('asset') as Promise<(Order & { asset: Asset })[]>;
+    //.populate(['asset', 'trade']);
   }
 
   findOne(id: string) {
---
+++ web-nextjs/src/app/orders/page.tsx
@@ -0,0 +1,67 @@
+import { Order } from "@/models"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeadCell,
+  TableRow
+} from "flowbite-react"
+import { AssetShow } from "@/components/AssetShow"
+import { OrderTypeBadge } from "@/components/OrderTypeBagde"
+import { OrderStatusBadge } from "@/components/OrderStatusBadge"
+
+export async function getOrders(walletId: string): Promise<Order[]> {
+  const response = await fetch(
+    `http://localhost:3000/orders?walletId=${walletId}`
+  )
+  return response.json()
+}
+
+export default async function OrdersListPage({
+  searchParams
+}: {
+  searchParams: Promise<{ wallet_id: string }>
+}) {
+  const { wallet_id } = await searchParams
+  const orders = await getOrders(wallet_id)
+
+  return (
+    <div className="flex flex-col space-y-5 flex-grow">
+      <article className="format">
+        <h1>Minhas ordens</h1>
+      </article>
+
+      <div className="overflow-x-auto w-full">
+        <Table className="w-full max-w-full table-fixed">
+          <TableHead>
+            <TableHeadCell>Ativo</TableHeadCell>
+            <TableHeadCell>Preço</TableHeadCell>
+            <TableHeadCell>Quantidade</TableHeadCell>
+            <TableHeadCell>Tipo</TableHeadCell>
+            <TableHeadCell>Status</TableHeadCell>
+          </TableHead>
+          <TableBody>
+            {orders.map((order, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell>
+                    <AssetShow asset={order.asset} />
+                  </TableCell>
+                  <TableCell>R$ {order.price}</TableCell>
+                  <TableCell>{order.shares}</TableCell>
+                  <TableCell>
+                    <OrderTypeBadge type={order.type} />
+                  </TableCell>
+                  <TableCell>
+                    <OrderStatusBadge status={order.status} />
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
+}
