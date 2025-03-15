732e65b8bcb20bc9168f2280b7e71af66254034f
flvsantos15 <flvsantos300@gmail.com>
2025-03-14 23:25:08 -0300

    feat: add socket integration with nesjs

---
+++ home-broker-api/src/orders/orders.gateway.ts
@@ -0,0 +1,28 @@
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OrderType } from './entities/order.entity';
+import { OrdersService } from './orders.service';
+
+type PayloadProps = {
+  assetId: string;
+  walletId: string;
+  type: OrderType;
+  shares: number;
+  price: number;
+};
+
+@WebSocketGateway()
+export class OrdersGateway {
+  constructor(private ordersService: OrdersService) {}
+
+  @SubscribeMessage('orders/create')
+  async handleMessage(client: any, payload: PayloadProps) {
+    const order = await this.ordersService.create({
+      assetId: payload.assetId,
+      walletId: payload.walletId,
+      type: payload.type,
+      shares: payload.shares,
+      price: payload.price,
+    });
+    return order;
+  }
+}
