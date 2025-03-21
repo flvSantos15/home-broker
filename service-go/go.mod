645275b3e4a1067c0c1fa0d7db5f2636ee07f73f
flvsantos15 <flvsantos300@gmail.com>
2025-03-20 21:53:26 -0300

    chore: add order_processor struct

--- service-go/go.mod
+++ service-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/flvsantos15/home-broker/service-go
 
 go 1.23.4
+
+require github.com/google/uuid v1.6.0
--- service-go/internal/market/entity/transaction.go
+++ service-go/internal/market/entity/transaction.go
@@ -1,3 +1,36 @@
 package entity
 
-type Transaction struct{}
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Parei no 52:27
+
+type Transaction struct {
+	ID           string
+	SellingOrder *Order
+	BuyingOrder  *Order
+	Shares       int
+	Price        float64
+	Total        float64
+	DateTime     time.Time
+}
+
+func NewTransaction(sellingOrder *Order, burOrder *Order, shares int, price float64) *Transaction {
+	return &Transaction{
+		ID:           uuid.New().String(),
+		SellingOrder: sellingOrder,
+		BuyingOrder:  burOrder,
+		Shares:       shares,
+		Price:        price,
+		Total:        price * float64(shares),
+		DateTime:     time.Now(),
+	}
+}
+
+func (t *Transaction) Process() {
+	processor := NewOrderProcessor(t)
+	processor.Process()
+}
